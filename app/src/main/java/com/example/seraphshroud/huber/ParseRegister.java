bbbf74116788abf52da23e36c08e3f6748ffb3e5
Brian Lam <crazy727@gmail.com>
2015-10-29 19:24:08 -0700

    Can finally connect to the Database

--- app/src/main/java/com/example/seraphshroud/huber/ParseRegister.java
+++ app/src/main/java/com/example/seraphshroud/huber/ParseRegister.java
@@ -2,23 +2,20 @@ package com.example.seraphshroud.huber;
 
 import android.app.Application;
 import com.parse.Parse;
-import com.parse.ParseClassName;
 import com.parse.ParseObject;
-
 
 /**
  * Created by SeraphShroud on 10/29/2015.
  */
-@ParseClassName("ParseRegister")
 public class ParseRegister extends Application {
 
     @Override
     public void onCreate(){
         super.onCreate();
-        // Enable Local Datastore.
-        Parse.enableLocalDatastore(this);
-        //This is what Parse was complaining about
-        ParseObject.registerSubclass(Client.class);
+        // Do not enable this or it will not connect to the database.
+        // Parse.enableLocalDatastore(this);
+        // ParseObject.registerSubclass(Client.class);
+
         Parse.initialize(this, "ENaimFMoqvCB6RgKMgPR8VD60OupUmE10R4atss5", "LqBZwLYBka8JOvGZKXvuBE91Ul5xbHnfq0ZrSqgY");
 
     }
