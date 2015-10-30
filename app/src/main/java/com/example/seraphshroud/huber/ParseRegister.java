f60915c317a4f1484ef349f37088fd6d4eda393d
Brian Lam <crazy727@gmail.com>
2015-10-29 17:59:59 -0700

    Added Client.java

--- app/src/main/java/com/example/seraphshroud/huber/ParseRegister.java
+++ app/src/main/java/com/example/seraphshroud/huber/ParseRegister.java
@@ -3,6 +3,7 @@ package com.example.seraphshroud.huber;
 import android.app.Application;
 import com.parse.Parse;
 import com.parse.ParseClassName;
+import com.parse.ParseObject;
 
 
 /**
@@ -17,6 +18,7 @@ public class ParseRegister extends Application {
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
         //This is what Parse was complaining about
+        ParseObject.registerSubclass(Client.class);
         Parse.initialize(this, "ENaimFMoqvCB6RgKMgPR8VD60OupUmE10R4atss5", "LqBZwLYBka8JOvGZKXvuBE91Ul5xbHnfq0ZrSqgY");
 
     }
